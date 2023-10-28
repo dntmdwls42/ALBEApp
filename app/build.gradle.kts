5e73547b386a33b8407d910c3e7866b8a32defd7
mnyu123 <whdudwo5582@gmail.com>
2023-10-28 17:10:10 +0900

    설정 레이아웃 추가 , 폰트추가 , sdk버전 34로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "kr.ac.wku.albeapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "kr.ac.wku.albeapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
