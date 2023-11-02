5755a346eb6ec660f0234b4a2096f6d0e1528d60
mnyu123 <whdudwo5582@gmail.com>
2023-11-02 18:13:11 +0900

    (임시) 메인 레이아웃 구성 삭제 , 이미지 업로드 레이아웃 이동 작성

--- app/src/main/java/kr/ac/wku/albeapp/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/albeapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package kr.ac.wku.albeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.albeapp.databinding.ActivityMainBinding
 
 // 초기 로그인 화면 액티비티
 class MainActivity : AppCompatActivity() {
+    // "메인 페이지" 데이터 바인딩 세팅 1
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // "메인 페이지" 데이터 바인딩 세팅 2
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        
+        binding.fromImagePage.setOnClickListener { 
+            // 테스트용 이미지 업로드 레이아웃으로 이동하는 이벤트 , 테스트용
+
+            // 화면 이동 :  intent
+            // imageupload 레이아웃이 도착지로 설정함.
+            
+            var myIntent = Intent(this, ImageUpload::class.java)
+            
+            // 이미지 업로드 레이아웃으로 이동
+            startActivity(myIntent)
+        }
     }
 }
