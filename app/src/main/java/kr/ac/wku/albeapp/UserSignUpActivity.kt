68999d53e8eeda90d8033e75e07c339b93bfb095
mnyu123 <whdudwo5582@gmail.com>
2023-11-02 17:35:44 +0900

    회원가입 페이지 레이아웃 수정

--- app/src/main/java/kr/ac/wku/albeapp/UserSignUpActivity.kt
+++ app/src/main/java/kr/ac/wku/albeapp/UserSignUpActivity.kt
@@ -2,12 +2,11 @@ package kr.ac.wku.albeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kr.ac.wku.albeapp.R
 
 // 로그인 후 메인화면 액티비티
 class userViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_view)
+        setContentView(R.layout.activity_user_signup)
     }
 }
