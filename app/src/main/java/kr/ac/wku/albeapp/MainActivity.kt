b9d907ce0cb861b1f08636cb77d24c183b4fd412
mnyu123 <whdudwo5582@gmail.com>
2023-11-04 13:30:59 +0900

    테스트 버전 삭제

--- app/src/main/java/kr/ac/wku/albeapp/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/albeapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package kr.ac.wku.albeapp
 
+import android.Manifest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import kr.ac.wku.albeapp.databinding.ActivityMainBinding
+import kr.ac.wku.albeapp.setting.SettingActivity
 
 // 초기 로그인 화면 액티비티
 class MainActivity : AppCompatActivity() {
@@ -21,10 +24,16 @@ class MainActivity : AppCompatActivity() {
             // 화면 이동 :  intent
             // imageupload 레이아웃이 도착지로 설정함.
             
-            var myIntent = Intent(this, ImageUpload::class.java)
+            var myIntent = Intent(this, SettingActivity::class.java)
             
             // 이미지 업로드 레이아웃으로 이동
             startActivity(myIntent)
         }
+
+        // 맨 처음에 앱에서 갤러리에 접근을 허용할것인지 물어보는
+        // 권한 체크 메시지 - 한번 허용하면 앱 데이터를 삭제하지 않는 이상 다시 뜨지 않음.
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+
     }
 }
