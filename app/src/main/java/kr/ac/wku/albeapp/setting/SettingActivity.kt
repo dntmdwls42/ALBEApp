7651786f591b5e622e9421d4c8286848f8cbd83d
dntmdwls42 <dntmdwls42@wku.ac.kr>
2023-11-05 16:11:59 +0900

    중요설정 토글 버튼을 활성화 해야 계정 탈퇴 버튼이 나오도록 변경했습니다.

--- app/src/main/java/kr/ac/wku/albeapp/setting/SettingActivity.kt
+++ app/src/main/java/kr/ac/wku/albeapp/setting/SettingActivity.kt
@@ -1,6 +1,9 @@
 package kr.ac.wku.albeapp.setting
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import kr.ac.wku.albeapp.R
 
@@ -9,5 +12,18 @@ class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting)
+
+        val moreSettingSwitch = findViewById<Switch>(R.id.moresetting)
+        val userDeleteButton = findViewById<Button>(R.id.userdelete)
+
+        userDeleteButton.visibility = View.GONE //회원 탈퇴 버튼은 숨겨져 있음(공간차지 x)
+
+        moreSettingSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) { //버튼이 켜지면
+                userDeleteButton.visibility = View.VISIBLE // 탈퇴버튼 표시
+            } else { //버튼이 꺼져 있다면
+                userDeleteButton.visibility = View.GONE // 탈퇴버튼 숨김상태
+            }
+        }
     }
 }
