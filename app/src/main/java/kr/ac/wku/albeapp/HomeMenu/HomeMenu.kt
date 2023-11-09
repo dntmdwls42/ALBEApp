e920d82a47ee30f955d3909615c4be2362ddb383
dntmdwls42 <dntmdwls42@wku.ac.kr>
2023-11-09 18:01:27 +0900

    홈 화면 제작 및 레이아웃 초기구성

---
+++ app/src/main/java/kr/ac/wku/albeapp/HomeMenu/HomeMenu.kt
@@ -0,0 +1,12 @@
+package kr.ac.wku.albeapp.HomeMenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.wku.albeapp.R
+
+class HomeMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_menu)
+    }
+}
