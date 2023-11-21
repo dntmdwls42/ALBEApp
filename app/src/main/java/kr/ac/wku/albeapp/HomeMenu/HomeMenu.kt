a90910f50956b2f6140191af7ab65d8ec29a755f
mnyu123 <whdudwo5582@gmail.com>
2023-11-21 22:50:41 +0900

    파이어베이스에서 홈 메뉴로 사용자이름 가져오기 성공함

--- app/src/main/java/kr/ac/wku/albeapp/HomeMenu/HomeMenu.kt
+++ app/src/main/java/kr/ac/wku/albeapp/HomeMenu/HomeMenu.kt
@@ -1,6 +1,7 @@
 package kr.ac.wku.albeapp.HomeMenu
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -23,38 +24,61 @@ class HomeMenu : AppCompatActivity() {
         // 파이어베이스 스토리지에서 참조
         storage = FirebaseStorage.getInstance()
 
+        val userPhoneNumber = intent.getStringExtra("phoneNumber")
+        val userRef = database.child("users").child(userPhoneNumber!!)
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val userName = dataSnapshot.child("userName").value as? String
+                val userStatus = dataSnapshot.child("userStatus").value as? Int
+
+                // 화면에 사용자 정보를 표시합니다.
+                findViewById<TextView>(R.id.home_username).text = userName
+                findViewById<TextView>(R.id.home_userphonenumber).text = userPhoneNumber
+                findViewById<TextView>(R.id.home_userstatus_text).text =
+                    if (userStatus == 1) "활성 상태" else "비활성 상태"
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                println("로그인 한 사용자 정보 받기 실패: ${databaseError.toException()}")
+            }
+        })
+
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val friendList = mutableListOf<Friendlist.Friend>()
 
                 // Firebase에서 가져온 데이터를 사용해 Friend 객체 리스트를 만듭니다.
                 for (userSnapshot in dataSnapshot.children) {
+                    val phoneNumber = userSnapshot.key
                     val userName = userSnapshot.child("userName").value as? String
                     val userPhoneNumber = userSnapshot.child("userPhoneNumber").value as? String
                     val userStatus = userSnapshot.child("userStatus").value as? Int
+                    if (phoneNumber != userPhoneNumber) {
+                        // Firebase 스토리지에서 이미지 URL을 가져옵니다.
+                        val imageRef = storage.getReference().child("image/$userPhoneNumber")
+                        imageRef.downloadUrl.addOnSuccessListener { uri ->
+                            val imageUrl = uri.toString()
 
-                    // Firebase 스토리지에서 이미지 URL을 가져옵니다.
-                    val imageRef = storage.getReference().child("image/$userPhoneNumber")
-                    imageRef.downloadUrl.addOnSuccessListener { uri ->
-                        val imageUrl = uri.toString()
+                            val friend =
+                                Friendlist.Friend(imageUrl, userName, userPhoneNumber, userStatus)
+                            friendList.add(friend)
 
-                        val friend = Friendlist.Friend(imageUrl, userName, userPhoneNumber, userStatus)
-                        friendList.add(friend)
-
-                        // RecyclerView에 어댑터를 설정합니다.
-                        val adapter = FriendListAdapter(friendList)
-                        val recyclerView: RecyclerView = findViewById(R.id.home_friendlist_recyclerview)
-                        recyclerView.layoutManager = LinearLayoutManager(this@HomeMenu)
-                        recyclerView.adapter = adapter
-                    }.addOnFailureListener {
-                        // 이미지 URL을 가져오는 데 실패했습니다.
+                            // RecyclerView에 어댑터를 설정합니다.
+                            val adapter = FriendListAdapter(friendList)
+                            val recyclerView: RecyclerView =
+                                findViewById(R.id.home_friendlist_recyclerview)
+                            recyclerView.layoutManager = LinearLayoutManager(this@HomeMenu)
+                            recyclerView.adapter = adapter
+                        }.addOnFailureListener {
+                            // 이미지 URL을 가져오는 데 실패했습니다.
+                        }
                     }
                 }
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
                 // Firebase에서 데이터를 가져오는 데 실패했습니다.
-                println("Failed to get data from Firebase: ${databaseError.toException()}")
+                println("다른 사용자 정보 받기 실패..: ${databaseError.toException()}")
             }
         })
     }
