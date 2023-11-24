b98a52c253d59a91349e2c327fc437f3693148e8
mnyu123 <whdudwo5582@gmail.com>
2023-11-24 21:50:01 +0900

    친구추가 이벤트 추가 , 데이터베이스에서 조회

--- app/src/main/java/kr/ac/wku/albeapp/HomeMenu/HomeMenu.kt
+++ app/src/main/java/kr/ac/wku/albeapp/HomeMenu/HomeMenu.kt
@@ -3,11 +3,14 @@ package kr.ac.wku.albeapp.HomeMenu
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -94,9 +97,11 @@ class HomeMenu : AppCompatActivity() {
                     ACTIVE -> {
                         findViewById<TextView>(R.id.home_userstatus_text).text = "활성 상태"
                     }
+
                     INACTIVE -> {
                         findViewById<TextView>(R.id.home_userstatus_text).text = "비활성 상태"
                     }
+
                     TEMP_INACTIVE -> {
                         findViewById<TextView>(R.id.home_userstatus_text).text = "일시적 비활성 상태"
                     }
@@ -160,7 +165,77 @@ class HomeMenu : AppCompatActivity() {
                 // Firebase에서 데이터를 가져오는 데 실패했습니다.
                 println("다른 사용자 정보 받기 실패..: ${databaseError.toException()}")
             }
+
         })
+
+        // 친구 추가 버튼인 이미지 버튼을 눌렀을때 생기는 이벤트
+        binding.addFriend.setOnClickListener {
+            // 다이얼로그 생성
+            val builder = AlertDialog.Builder(this@HomeMenu)
+            builder.setTitle("친구 추가")
+
+            // 입력 필드 설정
+            val input = EditText(this@HomeMenu)
+            input.inputType = InputType.TYPE_CLASS_PHONE  // 입력 타입을 전화번호로 설정
+            builder.setView(input)
+
+            // 검색 버튼을 눌렀을 때의 동작 설정
+            builder.setPositiveButton("검색") { dialog, _ ->
+                val searchPhoneNumber = input.text.toString()
+
+                database.child("users").child(searchPhoneNumber).addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            val userName = dataSnapshot.child("userName").value as? String
+                            val userStatus = dataSnapshot.child("userState").value as? Int
+
+                            // 검색 결과를 다이얼로그로 보여줌
+                            showSearchResultDialog(userName, userStatus)
+                        } else {
+                            Toast.makeText(this@HomeMenu, "해당 번호를 가진 사용자를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        Toast.makeText(this@HomeMenu, "데이터 검색에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                })
+                dialog.dismiss()
+            }
+
+            // 취소 버튼을 눌렀을 때의 동작 설정
+            builder.setNegativeButton("취소") { dialog, _ -> dialog.cancel() }
+
+            builder.show()
+        }
+    }
+
+    // 사용자 검색 결과를 보여주는 함수
+    // userName: 검색한 사용자의 이름
+    // userStatus: 검색한 사용자의 상태
+    private fun showSearchResultDialog(userName: String?, userStatus: Int?) {
+        // AlertDialog.Builder를 사용하여 다이얼로그를 만듬
+        val builder = AlertDialog.Builder(this@HomeMenu)
+        // 다이얼로그의 제목을 설정
+        builder.setTitle("검색 결과")
+
+        // 검색한 사용자의 이름과 상태를 문자열로 만들어 메시지로 설정
+        // 사용자의 상태는 ACTIVE, INACTIVE, TEMP_INACTIVE 중 하나밖에 없잖아
+        // when을 사용하여 각 상태에 해당하는 문자열을 만듬
+        val message = "이름: $userName\n상태: ${when (userStatus) {
+            ACTIVE -> "활성"
+            INACTIVE -> "비활성"
+            TEMP_INACTIVE -> "일시적 비활성"
+            else -> "알 수 없음"
+        }}"
+
+        // 만든 메시지를 다이얼로그에 설정함.
+        builder.setMessage(message)
+        // 다이얼로그에 '확인' 버튼을 추가하고, 이 버튼을 누르면 다이얼로그가 닫히도록 설정함
+        builder.setPositiveButton("확인") { dialog, _ -> dialog.dismiss() }
+
+        // 설정이 완료된 다이얼로그를 보여준다
+        builder.show()
     }
 
     private fun getFriendList(imageUrl: String): List<Friendlist.Friend> {
