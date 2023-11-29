d59123f7cb5ea5b06e62e6e13d8397067d45e5dc
mnyu123 <whdudwo5582@gmail.com>
2023-11-29 18:42:57 +0900

    유저 상태 클래스 내용 추가

--- app/src/main/java/kr/ac/wku/albeapp/logins/UserStatus.kt
+++ app/src/main/java/kr/ac/wku/albeapp/logins/UserStatus.kt
@@ -4,11 +4,12 @@ package kr.ac.wku.albeapp.logins
 enum class UserStatus(val status: Int, val description: String) {
     ACTIVE(1, "활성"),
     INACTIVE(0, "비활성"),
-    TEMP_INACTIVE(2, "일시적 비활성");
+    TEMP_INACTIVE(2, "일시적 비활성"),
+    NOTHING(3,"알수없음");
 
     companion object {
         fun fromStatus(status: Int): UserStatus {
-            return values().firstOrNull { it.status == status } ?: INACTIVE
+            return values().firstOrNull { it.status == status } ?: NOTHING
         }
     }
 }
